3909d546684bac67b4805e40fd9106ae22df14e2
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-03-14 16:34:09 -0500

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroFormComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
